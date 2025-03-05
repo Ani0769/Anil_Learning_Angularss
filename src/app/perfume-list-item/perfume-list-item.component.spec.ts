6496e6fe51bf54f65ed622f09bb9a3511a13b645
AnilKumar <w0769190@myscc.ca>
2025-03-05 01:41:34 -0500

    made new components

---
+++ src/app/perfume-list-item/perfume-list-item.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// @ts-ignore
+import { PerfumesListItemComponent } from './perfumes-list-item.component';
+
+describe('PerfumesListItemComponent', () => {
+  let component: PerfumesListItemComponent;
+  let fixture: ComponentFixture<PerfumesListItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PerfumesListItemComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(PerfumesListItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/perfume-list/perfume-list.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// @ts-ignore
+import { PerfumesListComponent } from './perfumes-list.component';
+
+describe('PerfumesListComponent', () => {
+  let component: PerfumesListComponent;
+  let fixture: ComponentFixture<PerfumesListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PerfumesListComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(PerfumesListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
