5aefc554611b15968558ba138b424b9c455322d2
AnilKumar <w0769190@myscc.ca>
2025-03-11 00:12:55 -0400

    step 9 and 10, done with assignment

---
+++ src/app/service/perfume.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {Observable, of} from "rxjs";
+import {Perfumes} from "../Shared/perfumes";
+import {perfumesList} from "../data/mock-content";
+@Injectable({
+  providedIn: 'root'
+})
+export class PerfumeService {
+
+  constructor() { }
+
+  getPerfumes(): Observable<Perfumes[]> {
+    return of(perfumesList);
+  }
+
+  getPerfumeById(id: number): Observable<Perfumes | undefined> {
+    const item = perfumesList.find(perfume => perfume.id === id);
+    return of(item);
+  }
+
+  addPerfume(newPerfume: Perfumes): Observable<Perfumes[]> {
+    perfumesList.push(newPerfume);
+    return of(perfumesList);
+  }
+
+  updatePerfume(updatedPerfume: Perfumes): Observable<Perfumes[]> {
+    const index = perfumesList.findIndex(item => item.id === updatedPerfume.id);
+    if (index !== -1) {
+      perfumesList[index] = updatedPerfume;
+    }
+    return of(perfumesList);
+  }
+
+  deletePerfumeById(id: number): Observable<Perfumes | undefined> {
+    const index = perfumesList.findIndex(item => item.id === id);
+    if (index !== -1) {
+      const removedItem = perfumesList.splice(index, 1)[0];
+      return of(removedItem);
+    }
+    return of(undefined);
+  }
+}
