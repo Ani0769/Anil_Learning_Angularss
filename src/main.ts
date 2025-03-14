dd8a9f47aa271e2bafb026a865aac14d63545731
AnilKumar <w0769190@myscc.ca>
2025-03-14 01:29:16 -0400

    done with step 3,4 and 5

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter,Routes} from '@angular/router';
+import {PerfumesListComponent} from "./app/perfumes-list/perfumes-list.component";
+import {PerfumesListItemComponent} from "./app/perfumes-list-item/perfumes-list-item.component";
+import {ModifyListItemComponent} from "./app/modify-list-item/modify-list-item.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
+const routes: Routes=[
+  {path:'', redirectTo: '/perfumes', pathMatch: 'full'},
+  {path: 'perfumes' , component: PerfumesListComponent},
+  {path: 'perfumes/:id' , component: PerfumesListItemComponent},
+  {path: 'modify-list-item' , component: ModifyListItemComponent},
+  {path: '**' , component: PageNotFoundComponent},
+]
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
