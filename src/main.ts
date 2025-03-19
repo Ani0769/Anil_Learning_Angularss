f2c41436f13b743179140644a752d1f7d9e86b50
AnilKumar <w0769190@myscc.ca>
2025-03-19 13:44:00 -0400

    assignmetn 5 started

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {Routes} from '@angular/router';
+import {PerfumesListComponent} from './app/perfumes-list/perfumes-list.component';
 
+
+const routes: Routes = [
+  { path: 'perfumes', component: PerfumesListComponent },
+  {path: 'modify-product', component: ModifyListItemComponent},
+  {path: '**', component:PageNotFoundComponent}
+];
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
