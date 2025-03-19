b903fa53e6ba78f4b71512f063a82ae1e6c0f68a
AnilKumar <w0769190@myscc.ca>
2025-03-19 13:45:53 -0400

    added routes and made componet

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {Routes} from '@angular/router';
 import {PerfumesListComponent} from './app/perfumes-list/perfumes-list.component';
+import {ModifyListItemComponent} from './app/modify-list-item/modify-list-item.component';
+import {PageNotFoundComponent} from './app/page-not-found/page-not-found.component';
+
 
 
 const routes: Routes = [
