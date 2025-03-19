63c9666e3bff04be7d6281a9f6a3587e13290122
AnilKumar <w0769190@myscc.ca>
2025-03-19 14:49:21 -0400

    done untill step 9

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {Routes} from '@angular/router';
+import { provideRouter} from "@angular/router";
 import {PerfumesListComponent} from './app/perfumes-list/perfumes-list.component';
 import {ModifyListItemComponent} from './app/modify-list-item/modify-list-item.component';
 import {PageNotFoundComponent} from './app/page-not-found/page-not-found.component';
@@ -13,5 +14,6 @@ const routes: Routes = [
   {path: 'modify-product', component: ModifyListItemComponent},
   {path: '**', component:PageNotFoundComponent}
 ];
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'))
